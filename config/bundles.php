a2b63de0a81286c468783887b194cdf783a58da7
rohankoid <rohan.maleku@choobs.com>
2018-09-08 16:56:48 +0200

    local web server bundle installed for dev

--- config/bundles.php
+++ config/bundles.php
@@ -8,4 +8,5 @@ return [
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
 ];
