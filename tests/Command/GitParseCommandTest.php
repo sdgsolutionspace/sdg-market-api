325455d2e14530fda4bc5c47e19da73c03682476
Xoeseko <xoeseko@gmail.com>
2019-02-12 15:37:05 +0100

    Started writing tests for diagnosing why some repos don't work

--- tests/Command/GitParseCommandTest.php
+++ tests/Command/GitParseCommandTest.php
@@ -9,8 +9,35 @@
 namespace App\Tests\Command;
 
 use App\Command\GitParseCommand;
+use Doctrine\ORM\EntityManager;
+use Github\Client;
+use PHPUnit\Framework\TestCase;
 
-class GitParseCommandTest extends \PHPUnit_Framework_TestCase
+class GitParseCommandTest extends TestCase
 {
+
+  private $client;
+  private $cmd;
+  private $em;
+
+  protected function setUp(): void
+  {
+    parent::setUp();
+//    $this->client = new \GuzzleHttp\Client(['base_uri' => "http://127.0.0.1:8000/api/v1/"]);
+    $this->cmd = new GitParseCommand(new EntityManager(), new Client());
+  }
+
+  public function testGetAll()
+  {
+    $response = $this->client->get('git-projects');
+    $this->assertEquals(200, $response->getStatusCode());
+    $data = json_decode($response->getBody(true), true);
+    $this->assertGreaterThan(0, count($data));
+  }
+
+  public function testRepoWithSymbolInName()
+  {
+    $response = $this->cmd->parseGithubUrl('http://github.com/xoeseko/sdg.market');
+  }
 
 }
