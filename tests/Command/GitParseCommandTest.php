1e0215bfe73dcd33a9048adf0a56dd5b17018dc1
Xoeseko <xoeseko@gmail.com>
2019-02-12 15:24:18 +0100

    Removed pem backup file that accidentally slipped into the repo initially and started implementing a command test

---
+++ tests/Command/GitParseCommandTest.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: xoeseko
+ * Date: 2/10/2019
+ * Time: 1:23 PM
+ */
+
+namespace App\Tests\Command;
+
+use App\Command\GitParseCommand;
+
+class GitParseCommandTest extends \PHPUnit_Framework_TestCase
+{
+
+}
